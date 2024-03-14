440d210749d24e82f215751fa859cdd59d5c77af
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2024-03-14 05:11:21 +0000

    initial commit

---
+++ meme-generator/src/App.svelte
@@ -0,0 +1,10 @@
+<div class="grid h-screen place-items-center">
+  <div>
+    <h1 class="text-5xl">Meme Generator API</h1>
+    <h2 class="text-3xl">something idk</h2>
+    <form id="uploadForm" enctype="multipart/form-data">
+    <input type="file" id="fileInput" name="files[]" multiple accept="image/*,video/*"/>
+    <button class="bg-black p-2 rounded text-white" type="button" id="uploadButton">Upload Files</button>
+    </form>
+  </div>
+</div>
